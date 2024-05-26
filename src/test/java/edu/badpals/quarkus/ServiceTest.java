53e473b16d01567fb8f9539f77295c4ebdef1c40
DavBaquero <144956230+DavBaquero@users.noreply.github.com>
2024-05-26 18:17:08 +0200

    🛡️ test(ServiceTest.java)  : add new test

--- src/test/java/edu/badpals/quarkus/ServiceTest.java
+++ src/test/java/edu/badpals/quarkus/ServiceTest.java
@@ -134,7 +134,7 @@ public class ServiceTest {
 	 * Devuelve una lista con los pedidos de la usuaria 
 	 * con el nombre indicado, si existe.
      * Si no existe, devuelve una lista vacía.
-	 *//* 
+	 */
 
     @Test
 	public void test_carga_orden() {
@@ -147,7 +147,7 @@ public class ServiceTest {
 		// Assertions.assertThat(ordenes).allMatch(orden -> orden.getUser().getNombre().equalsIgnoreCase("Hermione"));
 		// Assertions.assertThat(ordenes).allMatch(orden -> orden.getItem().getNombre().equalsIgnoreCase("+5 Dexterity Vest"));
 	}
-    
+    /*
     @Test
 	public void test_carga_orden_no_existe() {
         Assertions.assertThat(servicio).isNotNull();
