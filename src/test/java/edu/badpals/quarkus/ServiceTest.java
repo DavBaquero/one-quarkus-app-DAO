4480137778c1ba720552087910c585df16b72118
DavBaquero <144956230+DavBaquero@users.noreply.github.com>
2024-05-26 12:12:15 +0200

    � fix : fix errors

--- src/test/java/edu/badpals/quarkus/ServiceTest.java
+++ src/test/java/edu/badpals/quarkus/ServiceTest.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.pingpong.onequarkusapp.dominio.Item;
-import org.pingpong.onequarkusapp.dominio.Orden;
-import org.pingpong.onequarkusapp.dominio.Usuaria;
+import edu.badpals.quarkus.*;
 
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
